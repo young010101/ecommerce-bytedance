cc2bfcde7469d0241adab78811da7a7af470561b
young010101_shinelon_windows <93481273+young010101@users.noreply.github.com>
2024-10-02 01:49:06 +0800

    完成alioss配置和文件上传代码

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,14 +1,20 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 /**
  * Common controller for admin.
@@ -18,15 +24,35 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @Api(tags = "公共接口")
 public class CommonController {
+
+    private final AliOssUtil aliOssUtil;
+
+    @Autowired
+    public CommonController(AliOssUtil aliOssUtil) {
+        this.aliOssUtil = aliOssUtil;
+    }
+
     /**
      * Upload file.
-     * @return The result.
+     *
      * @param file The file to upload.
+     * @return The result.
      */
-     @PostMapping("/upload")
-     @ApiOperation("上传文件")
-     public Result<?> upload(@RequestParam("file") MultipartFile file) { //TODO 参数名和前端一致
-         log.info("上传文件：{}", file);
-         return Result.success();
-     }
+    @PostMapping("/upload")
+    @ApiOperation("上传文件")
+    public Result<String> upload(@RequestParam("file") MultipartFile file) { //TODO 参数名和前端一致
+        log.info("上传文件：{}", file);
+
+        try {
+            String originalFilename = file.getOriginalFilename();
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String objectName = UUID.randomUUID() + extension;
+            String filepath = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(filepath);
+        } catch (IOException e) {
+            log.error("上传文件失败", e);
+        }
+
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
 }
