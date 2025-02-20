b91df04bd161c1a85ad774bea9ef1825229099f9
co <yc5@tju.edu.cn>
2025-02-20 22:06:28 +0800

    feat: springboot 3.3.8

--- sky-common/src/main/java/com/sky/utils/JwtUtil.java
+++ sky-common/src/main/java/com/sky/utils/JwtUtil.java
@@ -18,7 +18,7 @@ public class JwtUtil {
      * @param secretKey jwt秘钥
      * @param ttlMillis jwt过期时间(毫秒)
      * @param claims    设置的信息
-     * @return
+     * @return jwt token
      */
     public static String createJWT(String secretKey, long ttlMillis, Map<String, Object> claims) {
         // 指定签名的时候使用的签名算法，也就是header那部分
@@ -43,7 +43,7 @@ public class JwtUtil {
      *
      * @param secretKey jwt秘钥 此秘钥一定要保留好在服务端, 不能暴露出去, 否则sign就可以被伪造, 如果对接多个客户端建议改造成多个
      * @param token     加密后的token
-     * @return
+     * @return claims
      */
     public static Claims parseJWT(String secretKey, String token) {
         // 得到DefaultJwtParser
