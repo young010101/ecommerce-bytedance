e27ec77d2f6c8b0d1e9b777f93ea80ee1e2315f5
co <yc5@tju.edu.cn>
2025-02-21 20:21:45 +0800

    feat: dubbo

---
+++ sky-server/src/main/java/com/sky/service/DemoService.java
@@ -0,0 +1,11 @@
+package com.sky.service;
+
+public interface DemoService {
+    /**
+     * 返回字符串.
+     *
+     * @param name 字符串
+     * @return 返回字符串
+     */
+    String sayHello(String name);
+}
