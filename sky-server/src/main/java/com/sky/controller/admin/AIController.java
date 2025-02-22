6a81a43b99fa82036c8ec5a90c82c9e079ecdcd2
co <yc5@tju.edu.cn>
2025-02-22 18:59:20 +0800

    feat: siliconflow ai chat

--- sky-server/src/main/java/com/sky/controller/admin/AIController.java
+++ sky-server/src/main/java/com/sky/controller/admin/AIController.java
@@ -1,6 +1,5 @@
 package com.sky.controller.admin;
 
-import com.sky.properties.ProxyProperties;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,10 +12,11 @@ public class AIController {
 
   private final ChatClient chatClient;
 
-  public AIController(ProxyProperties proxy, ChatClient.Builder builder) {
+//  public AIController(ProxyProperties proxy, ChatClient.Builder builder) {
+  public AIController(ChatClient.Builder builder) {
 
-    System.setProperty("https.proxyHost", proxy.getHost());
-    System.setProperty("https.proxyPort", proxy.getPort());
+//    System.setProperty("https.proxyHost", proxy.getHost());
+//    System.setProperty("https.proxyPort", proxy.getPort());
     this.chatClient = builder.build();
   }
 
