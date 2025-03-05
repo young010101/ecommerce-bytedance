afb37b849d8d83e2ea09063d85a52b797d3e38ff
co <yc5@tju.edu.cn>
2025-03-05 21:58:55 +0800

    feat: order and orderDetail

---
+++ sky-aspect/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -0,0 +1,18 @@
+package com.sky.mapper;
+
+import com.sky.entity.OrderDetail;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface OrderDetailMapper {
+
+    /**
+     * 批量插入订单明细数据.
+     *
+     * @param orderDetails 订单明细
+     */
+    void insertBatch(List<OrderDetail> orderDetails);
+
+}
---
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,42 @@
+package com.sky.controller.user;
+
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderSubmitVO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 订单
+ */
+@RestController("userOrderController")
+@RequestMapping("/user/order")
+@Slf4j
+@Tag(name = "C端-订单接口")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    /**
+     * 用户下单.
+     *
+     * @param ordersSubmitDTO 订单提交数据
+     * @return 订单提交结果
+     */
+    @PostMapping("/submit")
+    @Operation(summary = "用户下单")
+    public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO ordersSubmitDTO) {
+        log.info("用户下单：{}", ordersSubmitDTO);
+        OrderSubmitVO orderSubmitVO = orderService.submitOrder(ordersSubmitDTO);
+        return Result.success(orderSubmitVO);
+    }
+
+}
